9d234d6ba09f0963ac09748d42507f767aa051ea
Valentin Schaar <valentin.schaar@web.de>
2024-04-07 20:25:18 +0000

    Switched from certbot to nginxproxy.

--- cert/dummy_cert.sh
+++
@@ -1,2 +0,0 @@
-openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout ./conf/archive/valentin-schaar.de/privkey.pem -out ./conf/archive/valentin-schaar.de/fullchain.pem -subj "/CN=localhost"
-openssl storeutl -certs ./conf/archive/valentin-schaar.de/fullchain.pem > ./conf/archive/valentin-schaar.de/cert.pem
